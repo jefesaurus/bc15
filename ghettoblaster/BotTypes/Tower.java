a07836eb85800f5c63b7cefbbcc9c23205b0c537
Geoffrey Lalonde <glalonde@mit.edu>
2015-01-08 15:42:19 -0800

    tower attacks

--- ghettoblaster/BotTypes/Tower.java
+++ ghettoblaster/BotTypes/Tower.java
@@ -1,8 +1,11 @@
 package ghettoblaster.BotTypes;
 
 import ghettoblaster.RobotPlayer.BaseBot;
+import battlecode.common.Direction;
 import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
 
 public class Tower extends BaseBot {
   public Tower(RobotController rc) {
@@ -10,6 +13,14 @@ public class Tower extends BaseBot {
   }
 
   public void execute() throws GameActionException {
+    RobotInfo[] enemies = getEnemiesInAttackingRange();
+
+    if (enemies.length > 0) {
+      // attack!
+      if (rc.isWeaponReady()) {
+        attackLeastHealthEnemy(enemies);
+      }
+    }
     rc.yield();
   }
 }
