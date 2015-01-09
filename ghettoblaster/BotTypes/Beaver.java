fc30c260f5d963cdb8d111e33b1d7ee857f7aa3c
Kayla Ngan <kngan@mit.edu>
2015-01-09 16:23:02 -0500

    first beaver builds miner factory

--- ghettoblaster/BotTypes/Beaver.java
+++ ghettoblaster/BotTypes/Beaver.java
@@ -22,15 +22,15 @@ public class Beaver extends BaseBot {
   }
 
   public void execute() throws GameActionException {
-    if (beaverId == 0 && minerFactoryBuilt == false) {
+    if (beaverId == 1 && minerFactoryBuilt == false) {
       for (int i=0; i<8; i++) {
-        if (rc.canMove(directions[i]) && rc.hasBuildRequirements(RobotType.MINERFACTORY)) {
+        if (rc.canMove(directions[i]) && rc.hasBuildRequirements(RobotType.MINERFACTORY) && rc.isCoreReady()) {
           rc.build(directions[i], RobotType.MINERFACTORY);
           minerFactoryBuilt = true;
         }
       }
     }
-    if (rc.isCoreReady()) {
+    else if (rc.isCoreReady()) {
       if (rc.getTeamOre() < 500) {
         // mine
         if (MINING_TURNS > 0) {
