da29c3c492df4583000c85efbef7ba11e2dddba3
Andrew Huang <kahuang@mit.edu>
2015-01-11 21:08:40 -0500

    ghettov2

--- ghetto_v2/BotTypes/MinerFactory.java
+++ ghetto_v2/BotTypes/MinerFactory.java
@@ -17,19 +17,13 @@ public class MinerFactory extends BaseBot {
   }
 
   public void execute() throws GameActionException {
-    int numMiners = rc.readBroadcast(Messaging.NUM_MINERS);
-//    System.out.println("miner goal: " + numMiners);
-//    System.out.println("core ready: " + rc.isCoreReady());
-//    System.out.println("has reqs: " + rc.hasBuildRequirements(RobotType.MINER));
-//    System.out.println("ore: " + rc.getTeamOre());
-
-
-    if (numMiners > 0 && rc.isCoreReady() && rc.hasSpawnRequirements(RobotType.MINER)) {
-
+    if (Messaging.dequeueMiner()) {
+      while (!rc.isCoreReady() || !rc.hasSpawnRequirements(RobotType.MINER)) {
+        rc.yield();
+      };
       Direction newDir = getSpawnDirection(RobotType.MINER);
       if (newDir != null) {
         rc.spawn(newDir, RobotType.MINER);
-        rc.broadcast(Messaging.NUM_MINERS, numMiners-1);
       }
     }
     rc.yield();
