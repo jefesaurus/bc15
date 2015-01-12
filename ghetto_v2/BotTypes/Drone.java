d7eebae1f9ad53243dfb55930984ba9eec003192
Andrew Huang <kahuang@mit.edu>
2015-01-11 20:23:03 -0500

    second version of bot

---
+++ ghetto_v2/BotTypes/Drone.java
@@ -0,0 +1,65 @@
+package ghetto_v2.BotTypes;
+
+import ghetto_v2.Messaging;
+import ghetto_v2.Nav;
+import ghetto_v2.Nav.Engage;
+import ghetto_v2.RobotPlayer.BaseBot;
+import ghetto_v2.RobotPlayer.MovingBot;
+import battlecode.common.Clock;
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
+
+public class Drone extends MovingBot {  
+
+  public Drone(RobotController rc) {
+    super(rc);
+  }
+  
+  public void setup() {
+    
+  }
+
+  protected MapLocation rallyPoint = null;
+  protected MovingBot.AttackMode mode = MovingBot.AttackMode.OFFENSIVE_SWARM;
+
+  public void execute() throws GameActionException {
+    currentEnemies = getEnemiesInAttackingRange();
+    
+    rallyPoint = Messaging.readRallyPoint();
+    mode = Messaging.getSoldierMode();
+    
+    switch (mode) {
+    case TOWER_DIVE:
+      if (currentEnemies.length > 0) {
+        if (rc.isWeaponReady()) {
+          if (rc.canAttackLocation(rallyPoint)) {
+            rc.attackLocation(rallyPoint);
+          } else {
+            attackLeastHealthEnemy(currentEnemies);
+          }
+        }
+      } else if (rc.isCoreReady()) {
+        if (rallyPoint != null) {
+          Nav.goTo(rallyPoint, Engage.TOWERS);
+        }
+      }
+      break;
+    default:
+      if (currentEnemies.length > 0) {
+        if (rc.isWeaponReady()) {
+          attackLeastHealthEnemy(currentEnemies);
+        }
+      } else if (rc.isCoreReady()) {
+        if (rallyPoint != null) {
+          Nav.goTo(rallyPoint, Engage.NONE);
+        }
+      }
+      break;
+    }
+
+    rc.yield();
+  }
+}
---
+++ ghetto_v2/BotTypes/HQ.java
@@ -0,0 +1,93 @@
+package ghetto_v2.BotTypes;
+
+import ghetto_v2.Cache;
+import ghetto_v2.Messaging;
+import ghetto_v2.SupplyDistribution;
+import ghetto_v2.RobotPlayer.BaseBot;
+import ghetto_v2.RobotPlayer.MovingBot;
+import battlecode.common.Clock;
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
+import battlecode.common.RobotType;
+
+public class HQ extends BaseBot {
+  private MapLocation[] enemyTowers;
+  private int towersLeft = 6;
+  private SupplyDistribution supply;
+  
+  public HQ(RobotController rc) {
+    super(rc);
+    this.supply = new SupplyDistribution(rc);
+  }
+  
+  public void setup() throws GameActionException {
+    MapLocation rallyPoint = this.myHQ;
+    Messaging.setRallyPoint(rallyPoint);
+  }
+
+  public void execute() throws GameActionException {
+    int numBeavers = rc.readBroadcast(Messaging.NUM_BEAVERS);
+    
+    // This checks which enemy towers are still alive and broadcasts it to save bytecode across the fleet
+    Messaging.setSurvivingEnemyTowers();
+    
+    // Attack enemies if possible.
+    RobotInfo[] enemies = getEnemiesInAttackingRange();
+    if (enemies.length > 0) {
+      if (rc.isWeaponReady()) {
+        attackLeastHealthEnemy(enemies);
+      }
+    }
+    
+    // Spawn if possible
+    if (rc.isCoreReady() && rc.hasSpawnRequirements(RobotType.BEAVER) && numBeavers < 1) {
+      Direction newDir = getSpawnDirection(RobotType.BEAVER);
+      if (newDir != null) {
+        rc.spawn(newDir, RobotType.BEAVER);
+        rc.broadcast(Messaging.NUM_BEAVERS, numBeavers + 1);
+      }
+    }
+
+    if (Clock.getRoundNum() >= 570 && Clock.getRoundNum() <600) {
+      supply.distributeBatteryHQ();
+    }
+    if (Clock.getRoundNum() >= 600) {
+      Messaging.setSoldierMode(MovingBot.AttackMode.TOWER_DIVE);
+      targetNearestEnemyTower();
+      
+    }
+    rc.yield();
+  }
+  
+  /*
+   * Senses enemy towers and sets the soldier rally point to the nearest one
+   */
+  private void targetNearestEnemyTower() throws GameActionException {
+    if (towersLeft <= 0) {
+      return;
+    }
+    enemyTowers = rc.senseEnemyTowerLocations();
+    towersLeft = enemyTowers.length;
+    
+    if (towersLeft > 0) {
+      double tempDist;
+
+      double closestDist = myHQ.distanceSquaredTo(enemyTowers[0]);
+      int closestIndex = 0;
+      for (int i = 1; i < towersLeft; i++) {
+        tempDist = myHQ.distanceSquaredTo(enemyTowers[i]);
+        if (tempDist < closestDist) {
+          closestDist = tempDist;
+          closestIndex = i;
+        }
+      }
+      Messaging.setRallyPoint(enemyTowers[closestIndex]);
+    } else {
+      Messaging.setRallyPoint(enemyHQ);
+
+    }
+  }
+}
