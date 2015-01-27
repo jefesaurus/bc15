2d77ce1a8b81ce33edd33dacf51ef4f074761e5a
Andrew Huang <kahuang@mit.edu>
2015-01-27 14:25:41 -0500

    added miner all in at roundLimit() - 300

--- rambo/BotTypes/Miner.java
+++ rambo/BotTypes/Miner.java
@@ -1,6 +1,9 @@
 package rambo.BotTypes;
 
+import rambo.Cache;
+import rambo.HibernateSystem;
 import rambo.Messaging;
+import rambo.MovingBot;
 import rambo.Nav;
 import rambo.SupplyDistribution;
 import rambo.Util;
@@ -28,7 +31,7 @@ public class Miner extends rambo.MovingBot {
   
   public static boolean requestedHighOreLoc = false;
   public static MapLocation destination = null;
-    
+  public static int allInPushRound;
     
   public Miner(RobotController rc) {
     super(rc);
@@ -42,6 +45,7 @@ public class Miner extends rambo.MovingBot {
       int chan = Messaging.getCountChannel(RobotType.MINER);
       MINER_ID = rc.readBroadcast(chan);
     }
+    allInPushRound = rc.getRoundLimit() - 300;
   }
   
   public boolean friendInAttackRange(RobotInfo[] enemiesInSightRange) {
@@ -119,10 +123,175 @@ public class Miner extends rambo.MovingBot {
     }
   }
   
+  public void doAllInPush() throws GameActionException {
+    currentEnemies = Cache.getEngagementEnemies();
+    
+    MapLocation rallyPoint = Messaging.readRallyPoint();
+    MovingBot.AttackMode mode = Messaging.getFleetMode();
+    MapLocation divable = Messaging.getDivable(rc.getLocation());
+    if (divable != null && rallyPoint != divable) {
+      mode = MovingBot.AttackMode.HELP_DIVE;
+    }
+    rc.setIndicatorString(2, "Mode: " + mode.name() + ", Rally point: " + rallyPoint);
+    if (HibernateSystem.manageHibernation(mode, currentEnemies, rallyPoint)) {
+      //rc.setIndicatorString(2, "hibernating");
+      return;
+    }
+    SupplyDistribution.manageSupply();
+
+    switch (mode) {
+    case HELP_DIVE:
+      if (currentEnemies.length > 0) {
+        RobotInfo[] attackableEnemies = Cache.getAttackableEnemies();
+        if (attackableEnemies.length > 0) {
+          if (rc.isWeaponReady()) {
+            if (rc.canAttackLocation(divable)) {
+              rc.attackLocation(divable);
+            } else {
+              attackLeastHealthEnemy(attackableEnemies);
+            }
+          }
+        } else {
+          if (rc.isCoreReady() && rallyPoint != null) {
+            Nav.goTo(divable, Engage.ONE_TOWER);
+          }
+        }
+      } else if (rc.isCoreReady()) {
+        if (rallyPoint != null) {
+          Nav.goTo(divable, Engage.ONE_TOWER);
+        }
+      }
+      break;
+    case SPLIT_PUSH:
+      if (rallyPoint.equals(new MapLocation(8209, 12025))) {
+        System.out.println("in split push");
+      }
+      //System.out.println("rallyPoint: " + rallyPoint);
+      rc.setIndicatorString(2, "Mode: " + mode.name() + ", Rally point: " + rallyPoint);
+
+      doOffensiveMicroSplit(currentEnemies, rallyPoint);
+      break;
+    case SAFE_TOWER_DIVE_SPLIT:
+      rc.setIndicatorString(2, "Mode: " + mode.name() + ", Rally point: " + rallyPoint);
+      if (currentEnemies.length > 0) {
+        RobotInfo[] attackableEnemies = Cache.getAttackableEnemies();
+        if (attackableEnemies.length > 0) {
+          if (rc.isWeaponReady()) {
+            if (rc.canAttackLocation(rallyPoint)) {
+              rc.attackLocation(rallyPoint);
+            } else {
+              attackLeastHealthEnemy(attackableEnemies);
+            }
+          }
+        } else {
+          if (rc.isCoreReady() && rallyPoint != null) {
+            Nav.goTo(rallyPoint, Engage.ONE_TOWER);
+          }
+        }
+      } else if (rc.isCoreReady()) {
+        if (rallyPoint != null) {
+          Nav.goTo(rallyPoint, Engage.ONE_TOWER);
+        }
+      }
+      break;
+    case UNSAFE_TOWER_DIVE_SPLIT:
+      if (rallyPoint.equals(new MapLocation(8209, 12025))) {
+        System.out.println("in un safe tower dive split");
+      }
+      //System.out.println("rallyPoint: " + rallyPoint);
+
+      rc.setIndicatorString(2, "Mode: " + mode.name() + ", Rally point: " + rallyPoint);
+
+      if (currentEnemies.length > 0) {
+        RobotInfo[] attackableEnemies = Cache.getAttackableEnemies();
+        if (attackableEnemies.length > 0) {
+          if (rc.isWeaponReady()) {
+            if (rc.canAttackLocation(rallyPoint)) {
+              rc.attackLocation(rallyPoint);
+            } else {
+              attackLeastHealthEnemy(attackableEnemies);
+            }
+          }
+        } else {
+          if (rc.isCoreReady() && rallyPoint != null) {
+            Nav.goTo(rallyPoint, Engage.ALL_TOWERS);
+          }
+        }
+      } else if (rc.isCoreReady()) {
+        if (rallyPoint != null) {
+          Nav.goTo(rallyPoint, Engage.ALL_TOWERS);
+        }
+      }
+      break;
+    case SAFE_TOWER_DIVE:
+      if (currentEnemies.length > 0) {
+        RobotInfo[] attackableEnemies = Cache.getAttackableEnemies();
+        if (attackableEnemies.length > 0) {
+          if (rc.isWeaponReady()) {
+            if (rc.canAttackLocation(rallyPoint)) {
+              rc.attackLocation(rallyPoint);
+            } else {
+              attackLeastHealthEnemy(attackableEnemies);
+            }
+          }
+        } else {
+          if (rc.isCoreReady() && rallyPoint != null) {
+            Nav.goTo(rallyPoint, Engage.ONE_TOWER);
+          }
+        }
+      } else if (rc.isCoreReady()) {
+        if (rallyPoint != null) {
+          Nav.goTo(rallyPoint, Engage.ONE_TOWER);
+        }
+      }
+      rc.setIndicatorString(1, "rally: " + rallyPoint + Clock.getRoundNum());
+      break;
+    case UNSAFE_TOWER_DIVE:
+      if (currentEnemies.length > 0) {
+        RobotInfo[] attackableEnemies = Cache.getAttackableEnemies();
+        if (attackableEnemies.length > 0) {
+          if (rc.isWeaponReady()) {
+            if (rc.canAttackLocation(rallyPoint)) {
+              rc.attackLocation(rallyPoint);
+            } else {
+              attackLeastHealthEnemy(attackableEnemies);
+            }
+          }
+        } else {
+          if (rc.isCoreReady() && rallyPoint != null) {
+            Nav.goTo(rallyPoint, Engage.ALL_TOWERS);
+          }
+        }
+      } else if (rc.isCoreReady()) {
+        if (rallyPoint != null) {
+          Nav.goTo(rallyPoint, Engage.ALL_TOWERS);
+        }
+      }
+      break;
+    case RALLYING:
+    case OFFENSIVE_SWARM:
+      doOffensiveMicro(currentEnemies, rallyPoint);
+      break;
+    case DEFEND_TOWERS:
+    case DEFENSIVE_SWARM:
+      doDefensiveMicro(currentEnemies, rallyPoint);
+      break;
+    case COUNTER_ATTACK:
+      doSneakyMove(rallyPoint);
+      break;
+    default:
+      System.out.println("No default behavior");
+      break;
+    }
+  }
+  
   public void execute() throws GameActionException {
     SupplyDistribution.manageSupply();
 //    selfPreservation();
-    
+    if (Clock.getRoundNum() > allInPushRound) {
+      doAllInPush();
+      return;
+    }
     if (destination!=null) {
       if (this.curLoc.distanceSquaredTo(destination) < 5) {
         destination = null;
